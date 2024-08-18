5fdd4d6a52f0e82c1a37ffdf54b2b18e50093d85
Selwyn Jayme <143252444+RandomSci@users.noreply.github.com>
2024-08-18 23:38:28 +0800

    1. Added some comments to "app.py" make it easier to understand. 2. Removed the comment "gwapo ako" in Home.html

--- app.py
+++ app.py
@@ -4,24 +4,24 @@ from Flask_links import locations
 app = Flask(__name__)
 
 @app.route("/")
-def Home():
-    return render_template("Home.html")
+def Home(): 
+    return render_template("Home.html") # Para sa Home page
 
 @app.route("/Register")
 def Teacher_Register():
-    return render_template("ForTeacherRegistration.html")
+    return render_template("ForTeacherRegistration.html") # Teacher Registration page
 
 @app.route("/Login")
-def Teacher_login():
-    return render_template("ForTeachersLogin.html")
+def Teacher_login(): 
+    return render_template("ForTeachersLogin.html") # Teacher Log In Page
 
 @app.route("/StudentRegister")
 def Student_Register():
-    return render_template("ForStudentRegistration.html")
+    return render_template("ForStudentRegistration.html") #Student Registration page
 
 @app.route("/StudentLogin")
 def Student_Login():
-    return render_template("ForStudentLogin.html")
+    return render_template("ForStudentLogin.html") # Student Log In Page
 
 
 
