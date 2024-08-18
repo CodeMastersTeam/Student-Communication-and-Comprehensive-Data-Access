df065a9ac10866a64b3240cffcccd9c708ffe992
Selwyn Jayme <143252444+RandomSci@users.noreply.github.com>
2024-08-18 23:06:41 +0800

    1. Split the app.py pythoon file into two files:  - app.py for direct links routing  - Flask_links.py for complex processes using flask.
    2. Made some changes in Teachers.css
      * Added some colors
      * Changed fonts
    
    3. Made some changes in Teachers.html
      * Added 2 buttons (register & Log In)

---
+++ Flask_links.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, render_template
+from Database import *
+
+app = Flask(__name__)
+
+# class Locations:
+#     def __init__(self, x):
+#         self.x = x
+# 
+#     def ruotes(self):
+#         return app.route(self.x)
+# 
+# 
+# x = Locations("/")
+# @x.ruotes()
+# def Home():
+#     return render_template("Teachers.html")
+
+def locations(app):
+
+    @app.route("/Registration_links", methods = ["POST", "GET"])
+    def Registration():
+        link = request.form.get("link")
+        if request.method == "POST":
+            if link == "Teacher":
+                return render_template("Teachers.html")
+            elif link == "Student":
+                return render_template("Students.html")
+        else:
+            return render_template("Home.html")
+
+
+    @app.route("/Inputs", methods = ["GET", "POST"])
+    def StudentRegistration():
+        if request.method == "POST":
+            firstname = str(request.form["firstname"])
+            middlename = str(request.form["middlename"])
+            lastname = str(request.form["lastname"])
+            age = int(request.form["age"])
+            address = str(request.form["address"])
+            sex = str(request.form["sex"])
+            cellphone_number = str(request.form["cellphone_number"])
+            Birth_date = str(request.form["Birth_date"])
+            Birth_place = str(request.form["Birth_place"])
+            Course = str(request.form["Course"])
+
+            ForStudentRegistration(
+                firstname, middlename, lastname, age, address, sex, 
+                cellphone_number, Birth_date, Birth_place, Course
+            )
+
+            x = "Registered Successfully!"
+            return render_template("ForStudentRegistration.html", x = x)
+
+        else:
+            x = "Registration failed!"
+            return render_template("ForStudentRegistration.html")
+    
+    @app.route("/")
+    def Continue_here():
+        pass # Change later
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 from flask import Flask, render_template, request
-from Database import *
+from Flask_links import locations
+
 app = Flask(__name__)
 
-# nagdugang comment
 @app.route("/")
 def Home():
     return render_template("Home.html")
-
-@app.route("/Registration_links", methods = ["POST", "GET"])
-def Registration():
-    link = request.form.get("link")
-    if request.method == "POST":
-        if link == "Teacher":
-            return render_template("Teachers.html")
-        elif link == "Student":
-            return render_template("Students.html")
-    else:
-        return render_template("Home.html")
 
 @app.route("/Register")
 def Teacher_Register():
@@ -34,31 +23,28 @@ def Student_Register():
 def Student_Login():
     return render_template("ForStudentLogin.html")
 
-@app.route("/Inputs", methods = ["GET", "POST"])
-def StudentRegistration():
-    if request.method == "POST":
-        firstname = str(request.form["firstname"])
-        middlename = str(request.form["middlename"])
-        lastname = str(request.form["lastname"])
-        age = int(request.form["age"])
-        address = str(request.form["address"])
-        sex = str(request.form["sex"])
-        cellphone_number = str(request.form["cellphone_number"])
-        Birth_date = str(request.form["Birth_date"])
-        Birth_place = str(request.form["Birth_place"])
-        Course = str(request.form["Course"])
 
-        ForStudentRegistration(
-            firstname, middlename, lastname, age, address, sex, 
-            cellphone_number, Birth_date, Birth_place, Course
-        )
-        
-        x = "Registered Successfully!"
-        return render_template("ForStudentRegistration.html", x = x)
-    
-    else:
-        x = "Registration failed!"
-        return render_template("ForStudentRegistration.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+locations(app)
 
 if __name__ == "__main__":
     app.run(debug = True)
